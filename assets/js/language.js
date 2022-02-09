4ecf609d053db9c8b287b11fa1579677f64c049d
Sebastián Ibagué Castro <sebastian.castroa25@gmail.com>
2022-02-09 16:38:40 -0500

    Portfolio Sebastian

---
+++ assets/js/language.js
@@ -0,0 +1,274 @@
+var dictionary = {
+    /*==================== NAV DICTIONARY  ====================*/
+    'Home':{
+        'en': 'Home',
+        'es': 'Inicio'
+    },
+    'About':{
+        'en': 'About',
+        'es': 'Acerca de'
+    },
+    'Skills':{
+        'en': 'Skills',
+        'es': 'Habilidades'
+    },
+    'Qualification':{
+        'en': 'Qualification',
+        'es': 'Calificación'
+    },
+    'Portfolio':{
+        'en': 'Portfolio',
+        'es': 'Portafolio'
+    },
+    'Contact':{
+        'en': 'Contact',
+        'es': 'Contacto'
+    },
+    /*==================== HOME DICTIONARY  ====================*/
+    'home-title':{
+        'en': "Hi, I'm Sebastián",
+        'es': 'Hola! Soy Sebastián'
+    },
+    'home-subtitle':{
+        'en': 'Systems Engineer and Frontend Developer.',
+        'es': 'Ingeniero de Sistemas y Desarrollador Frontend.'
+    },
+    'home-contactme':{
+        'en': 'Contact Me',
+        'es': 'Contáctame'
+    },
+    'home-scrolldown':{
+        'en': 'Scroll down',
+        'es': 'Desplazarse abajo'
+    },
+    /*==================== ABOUT DICTIONARY  ====================*/
+    'about-title':{
+        'en': 'About Me',
+        'es': 'Acerca de Mí'
+    },
+    'about-subtitle':{
+        'en': 'My introduction',
+        'es': 'Mi introducción'
+    },
+    'Name:':{
+        'en': 'Name:',
+        'es': 'Nombre:'
+    },
+    'Age:':{
+        'en': 'Age:',
+        'es': 'Edad:'
+    },
+    'Phone:':{
+        'en': 'Phone:',
+        'es': 'Teléfono:'
+    },
+    'Email:':{
+        'en': 'Email:',
+        'es': 'Correo:'
+    },
+    'Language:':{
+        'en': 'Language:',
+        'es': 'Idiomas:'
+    },
+    'Languages':{
+        'en': 'Spanish - English (Middle)',
+        'es': 'Español - Inglés (Intermedio)'
+    },
+    'about-description':{
+        'en': 'Frontend Developer and Systems Engineer, with knowledge of web technologies, programming languages and design.<br><br>I am a responsible person with ease of working individually and as a team, always focused on developing quality software.',
+        'es': 'Desarrollador Frontend e Ingeniero de Sistemas, con conocimientos en tecnologías web, lenguajes de programación y diseño.<br><br> Soy una persona responsable con facilidad para trabajar individualmente y en equipo, siempre enfocado en desarrollar software de calidad.'
+    },
+    'about-CV':{
+        'en': 'Download CV',
+        'es': 'Descargar CV'
+    },
+    /*==================== SKILLS DICTIONARY  ====================*/
+    'skills-title':{
+        'en': 'Skills',
+        'es': 'Habilidades'
+    },
+    'skills-subtitle':{
+        'en': 'My technical level',
+        'es': 'Mi nivel técnico'
+    },
+    'skill-title1':{
+        'en': 'Frontend Development',
+        'es': 'Desarrollo Frontend'
+    },
+    'skill-title2':{
+        'en': 'Backend Development',
+        'es': 'Desarrollo Backend'
+    },
+    'skill-title3':{
+        'en': 'Design',
+        'es': 'Diseño'
+    },
+    /*==================== QUALIFICATION DICTIONARY  ====================*/
+    'qualification-title':{
+        'en': 'Qualification',
+        'es': 'Calificación'
+    },
+    'qualification-subtitle':{
+        'en': 'My experience',
+        'es': 'Mi experiencia'
+    },
+    'Education':{
+        'en': 'Education',
+        'es': 'Educación'
+    },
+    'Work':{
+        'en': 'Work',
+        'es': 'Trabajo'
+    },
+    'Other Studies':{
+        'en': 'Other Studies',
+        'es': 'Otros Estudios'
+    },
+    'Online Courses':{
+        'en': 'Online Courses',
+        'es': 'Cursos Online'
+    },
+    'Certificates':{
+        'en': 'Certificates',
+        'es': 'Certificados'
+    },
+    'Today':{
+        'en': 'Today',
+        'es': 'Hoy'
+    },
+    'Read more':{
+        'en': 'Read more...',
+        'es': 'Leer mas...'
+    },
+    'Systems Engineer':{
+        'en': 'Systems Engineer',
+        'es': 'Ingeniero de sistemas'
+    },
+    'University':{
+        'en': 'Autonomous University of Manizales',
+        'es': 'Universidad Autónoma de Manizales'
+    },
+    'System Technique':{
+        'en': 'System Technique',
+        'es': 'Técnico en Sistemas'
+    },
+    'School':{
+        'en': 'Francisco José de Caldas Technical Institute',
+        'es': 'Instituto Técnico Francisco José de Caldas'
+    },
+    'Basic Education':{
+        'en': 'Basic, Middle and Higher education',
+        'es': 'Educación Básica, Media y Superior'
+    },
+    'Junior Developer':{
+        'en': 'Software Developer',
+        'es': 'Desarrollador de software'
+    },
+    'Functions':{
+        'en': 'Functions',
+        'es': 'Funciones'
+    },
+    'HF1':{
+        'en': 'Hardware and Software Technical Support.',
+        'es': 'Soporte Técnico de Hardware y Software.'
+    },
+    'HF2':{
+        'en': 'Analysis and diagnosis of the main software.',
+        'es': 'Análisis y diagnóstico del software principal.'
+    },
+    'HF3':{
+        'en': 'Diligence of the self-diagnosis tool for the implementation of the Digital Government Policy.',
+        'es': 'Diligenciamiento de la herramienta autodiagnostico para la implementación de la Política de Gobierno Digital.'
+    },
+    'BF1':{
+        'en': 'Implementation of new functions and features in its main software BNET ERP and in the different developments of the company.',
+        'es': 'Implementación de nuevas funciones y características en su software principal BNET ERP y en los diferentes desarrollos de la empresa.'
+    },
+    'BF2':{
+        'en': 'Correction of errors in the different applications of the company.',
+        'es': 'Corrección de errores en las diferentes aplicaciones de la empresa.'
+    },
+    'BF3':{
+        'en': 'Mockup Creation.',
+        'es': 'Creación de Mockups.'
+    },
+    'BF4':{
+        'en': 'Development of user interfaces.',
+        'es': 'Desarrollo de interfaces de usuario.'
+    },
+    'BF5':{
+        'en': 'Development of new web applications.',
+        'es': 'Desarrollo de nuevas aplicaciones web.'
+    },
+    /*==================== PORTFOLIO DICTIONARY  ====================*/
+    'portfolio-title':{
+        'en': 'Portfolio',
+        'es': 'Portafolio'
+    },
+    'portfolio-subtitle':{
+        'en': 'Most recent work',
+        'es': 'Trabajo mas reciente'
+    },
+    /*==================== CONTACT DICTIONARY  ====================*/
+    'Subject:':{
+        'en': 'Subject:',
+        'es': 'Asunto:'
+    },
+    'Message:':{
+        'en': 'Message:',
+        'es': 'Mensaje:'
+    },
+    'Send Message':{
+        'en': 'Send Message',
+        'es': 'Enviar Mensaje'
+    },
+    'Email':{
+        'en': 'Email',
+        'es': 'Correo'
+    },
+    'Location':{
+        'en': 'Location',
+        'es': 'Ubicación'
+    },    
+    'Call Me':{
+        'en': 'Call Me',
+        'es': 'Llámame'
+    },
+    'contact-subtitle':{
+        'en': 'Get in touch',
+        'es': 'Ponerse en contacto'
+    },
+    'footer-copy':{
+        'en': 'All right reserver.',
+        'es': 'Todos los derechos reservados.'
+    }
+};
+
+var langs = ['en', 'es'];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+var varTranslator = true;
+
+window.change_lang = function() {
+    current_lang_index = ++current_lang_index % 2;
+    current_lang = langs[current_lang_index];
+    translate();
+}
+
+function translate() {
+    var languageButton = document.getElementById('language-button');
+    var themeButton = document.getElementById('theme-button');
+    var inputSubmit = document.getElementById('input-submit');
+
+    varTranslator?languageButton.title = "Translate to spanish":languageButton.title = "Traducir a inglés";
+    varTranslator?themeButton.title = "Change theme":themeButton.title = "Cambiar de tema";
+    varTranslator?inputSubmit.value = "Send":inputSubmit.value = "Enviar";
+    varTranslator=!varTranslator;
+
+    $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
+
+translate();
